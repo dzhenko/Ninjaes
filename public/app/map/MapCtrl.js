c232d486213d2f5644138252b8efb0b4c47e0635
stiliankanchev <pechi169@gmail.com>
2014-10-05 07:01:07 +0300

    Added: castle building, moving player; Fixed: drawing the map after loading all images

--- public/app/map/MapCtrl.js
+++ public/app/map/MapCtrl.js
@@ -1,69 +1,98 @@
 ﻿'use strict';
 
-app.controller('MapCtrl', ['$scope', '$location', 'mapData', function ($scope, $location, mapData) {
+app.controller('MapCtrl', ['$scope', '$location', 'mapData', 'mapPreloader', function ($scope, $location, mapData, mapPreloader) {
     $scope.params = {};
-    $scope.vx = 0;
-    $scope.vy = 0;
+    $scope.playerPosition = {
+        x: 0,
+        y: 0
+    };
+    $scope.castlePosition = {
+        x: 0,
+        y: 0
+    };
+    $scope.mapPosition = {
+        x: 0,
+        y: 0
+    };
+    $scope.images = {};
+    var gameObjects = [
+        {name: 'castle', src: '../../img/castle-building.gif'},
+        {name: 'bluePlayer', src: '../../img/bluePlayer.png'},
+        {name: 'map', src: './../img/world.png'}
+    ];
     var distance = 60;
 
-    var handler = function(e){
+    var canvas = document.querySelector('canvas');
+    var ctx = canvas.getContext('2d');
+
+    mapPreloader.preloadImages(gameObjects)
+        .then(function (images) {
+            $scope.images = images;
+            $scope.playerPosition.x = canvas.width / 2;
+            $scope.playerPosition.y = canvas.height / 2;
+            $scope.redraw();
+        });
+
+    var keyboardHandler = function (e) {
         // Arrow key press navigates through the page by default
         // so this default should be prevented to move only on the map
         // using arrow keys
         e.preventDefault();
 
-        if(e.keyCode === 37) {
+        if (e.keyCode === 37) {
             // console.log('lef arrow');
-            $scope.vx += distance;
+            $scope.mapPosition.x += distance;
             $scope.redraw();
         }
-        if(e.keyCode === 38) {
+        if (e.keyCode === 38) {
             // console.log('up arrow');
-            $scope.vy += distance;
+            $scope.mapPosition.y += distance;
             $scope.redraw();
         }
-        if(e.keyCode === 39) {
+        if (e.keyCode === 39) {
             // console.log('right arrow');
-            $scope.vx -= distance;
+            $scope.mapPosition.x -= distance;
             $scope.redraw();
         }
-        if(e.keyCode === 40) {
+        if (e.keyCode === 40) {
             // console.log('down arrow');
-            $scope.vy -= distance;
+            $scope.mapPosition.y -= distance;
             $scope.redraw();
         }
     };
 
+    $scope.movePlayer = function (event) {
+        $scope.playerPosition.x = -$scope.mapPosition.x + event.offsetX;
+        $scope.playerPosition.y = -$scope.mapPosition.y + event.offsetY;
+        console.log($scope.playerPosition.x + ' ' + $scope.playerPosition.y);
+        $scope.redraw();
+    };
+
     var $doc = angular.element(document);
-    $doc.on('keydown', handler);
-    $scope.$on('$destroy',function(){
-        $doc.off('keydown', handler);
+    $doc.on('keydown', keyboardHandler);
+    $scope.$on('$destroy', function () {
+        $doc.off('keydown', keyboardHandler);
     });
 
-    var canvas = document.querySelector('canvas');
-    var ctx = canvas.getContext('2d');
-    var img = new Image();
-    img.src = './../img/world.png';
+    $scope.redraw = function () {
+        var vx = $scope.mapPosition.x % $scope.images.map.width;
+        var vy = $scope.mapPosition.y % $scope.images.map.height;
 
-    $scope.redraw = function(){
-        if (Math.abs($scope.vx) > img.width) {
-            $scope.vx = 0;
+        if (vx > 0) {
+            vx -= $scope.images.map.width;
         }
-        if (Math.abs($scope.vy) > img.height) {
-            $scope.vy=0;
+        if (vy > 0) {
+            vy -= $scope.images.map.height;
         }
-        if($scope.vx > 0){
-            $scope.vx -= img.width;
-        }
-        if($scope.vy > 0){
-            $scope.vy -= img.height;
-        }
-        var vx = $scope.vx;
-        var vy = $scope.vy;
 
-        ctx.drawImage(img, vx, vy);
-        ctx.drawImage(img, vx, img.height-Math.abs(vy));
-        ctx.drawImage(img, img.width-Math.abs(vx), vy);
-        ctx.drawImage(img, img.width-Math.abs(vx), img.height-Math.abs(vy));
+        ctx.drawImage($scope.images.map, vx, vy);
+        ctx.drawImage($scope.images.map, vx, $scope.images.map.height - Math.abs(vy));
+        ctx.drawImage($scope.images.map, $scope.images.map.width - Math.abs(vx), vy);
+        ctx.drawImage($scope.images.map, $scope.images.map.width - Math.abs(vx), $scope.images.map.height - Math.abs(vy));
+
+        var posX = $scope.playerPosition.x + $scope.mapPosition.x - distance;
+        var posY = $scope.playerPosition.y + $scope.mapPosition.y - distance;
+        ctx.drawImage($scope.images.castle, $scope.castlePosition.x + $scope.mapPosition.x, $scope.castlePosition.y + $scope.mapPosition.y);
+        ctx.drawImage($scope.images.bluePlayer, posX, posY);
     };
 }]);
---
+++ public/app/services/map/mapPreloader.js
@@ -0,0 +1,35 @@
+app.factory('mapPreloader', ['$q', function ($q) {
+    'use strict';
+
+    //http://www.javascriptkit.com/javatutors/preloadimagesplus.shtml
+    function preloadImages(arr) {
+        var defer = $q.defer();
+        var newimages = {},
+            loadedimages = 0;
+
+        function imageloadpost() {
+            loadedimages++;
+            if (loadedimages == arr.length) {
+                defer.resolve(newimages);
+            }
+        }
+
+        for (var i = 0; i < arr.length; i++) {
+            newimages[arr[i].name] = new Image();
+            newimages[arr[i].name].src = arr[i].src;
+            newimages[arr[i].name].onload = function () {
+                defer.notify(loadedimages);
+                imageloadpost()
+            };
+            newimages[arr[i].name].onerror = function () {
+                imageloadpost()
+            }
+        }
+
+        return defer.promise;
+    }
+
+    return {
+        preloadImages: preloadImages
+    }
+}]);
