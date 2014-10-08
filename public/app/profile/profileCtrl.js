1f201bf3fb5b7ce11cd3f108ccc2457acb6ae38d
digitalbeas7 <zarichepower@gmail.com>
2014-10-08 19:26:21 +0300

    profile page update

--- public/app/profile/profileCtrl.js
+++ public/app/profile/profileCtrl.js
@@ -1,5 +1,9 @@
 'use strict';
 
-app.controller('ProfileCtrl', ['$scope', function ($scope) {
-
+app.controller('ProfileCtrl', ['$scope', 'identity', function ($scope, identity) {
+    $scope.userInfo = {
+        username: identity.currentUser.username,
+        firstName: identity.currentUser.firstName,
+        lastName: identity.currentUser.lastName
+    };
 }]);
