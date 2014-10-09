022effe5eba217a512afad4eb8207e962edc314f
digitalbeas7 <zarichepower@gmail.com>
2014-10-09 17:27:10 +0300

    updated castle

--- public/app/castle/CastleCtrl.js
+++ public/app/castle/CastleCtrl.js
@@ -1,5 +1,20 @@
 'use strict';
 
 app.controller('CastleCtrl', ['$scope', '$location', function ($scope, $location) {
+    $scope.castle = angular.element($(".castle"));
+    $scope.bgSize = '1000px 790px';
 
+    var background = 'url("../../img/insideCastle.jpg")';
+    var backgroundBuild = 'url("../../img/insideCastle-build.jpg")';
+    var backgroundTroops = 'url("../../img/insideCastle-troops.jpg")';
+
+    $scope.bgInit = function(){
+        $scope.background = background;
+    };
+    $scope.getBuildBackground = function(){
+        $scope.background = backgroundBuild;
+    };
+    $scope.getTroopsBackground = function(){
+        $scope.background = backgroundTroops;
+    };
 }]);
