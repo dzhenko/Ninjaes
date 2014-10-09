629ec78e7502a77dee627b7f9fa4f4c4584c3be9
digitalbeas7 <zarichepower@gmail.com>
2014-10-09 16:05:23 +0300

    settings page

--- public/app/settings/settingsCtrl.js
+++ public/app/settings/settingsCtrl.js
@@ -1,5 +1,15 @@
 'use strict';
 
-app.controller('SettingsCtrl', ['$scope', function ($scope) {
+app.controller('SettingsCtrl', ['$scope', '$location', 'auth', 'notifier', function ($scope, $location, auth, notifier) {
 
+    $scope.user = {};
+
+    $scope.update = function (user) {
+        auth.update(user).then(function () {
+            notifier.success('Changes were successful!');
+
+        }, function() {
+            notifier.error('Error!');
+        });
+    };
 }]);
