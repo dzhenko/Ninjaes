7d7a27516d0a2dccb7db7a1f2c2b4d42339a890b
Dimitar Mitev <dnmitev@gmail.com>
2014-10-10 08:59:55 +0300

    18 passing tests so far, so good. I'm still writing tests

--- server/tests/troopsHandlerTests.js
+++ server/tests/troopsHandlerTests.js
@@ -2,84 +2,113 @@
 
 /*globals describe, it*/
 
-var  troopsHandler = require('../handlers/troopsHandler'),
-    expect = require('chai').expect;
+var troopsHandler = require('../handlers/troopsHandler'),
+    expect = require('chai').expect,
+    gameData = require('../data/gameData'),
+    fakeInfo = {};
 
-describe('Check', function() {
+describe('troopsHandler', function () {
+    beforeEach(function () {
+        fakeInfo.user = {
+            username: 'pesho',
+            coordinates: {
+                x: 50,
+                y: 50
+            },
+            gold: 5000,
+            movement: 2000,
+            experience: 100,
+            troops: [28, 5, 3, 0, 0, 0]
+        };
 
-    it('if handler returns false when information is an empty object', function() {
-        var emptyInformationObj = {};
-        var actual = troopsHandler.buyTroops(emptyInformationObj).success;
-
-        expect(actual).to.equal(false);
+        gameData.players.get = function () {
+            return fakeInfo.user;
+        }
     });
 
-    it('if handler returns false when information has no request property', function() {
-        var informationObjWithoutRequestProp = {  user: { gold: 25000, troops: [10, 15, 2, 1, 3, 4, 0]}, castle: {troopsForSale: [100, 20, 10, 30, 8, 2]}};
-        var actual = troopsHandler.buyTroops(informationObjWithoutRequestProp).success;
+    describe('#buyTroops', function () {
 
-        expect(actual).to.equal(false);
-    });
 
-    it('if handler returns false when information has no user property', function(){
-        var informationObjWithoutUserProp = { request: { index: 1, amount: 5}, castle: {troopsForSale: [100, 20, 10, 30, 8, 2]}};
-        var actual = troopsHandler.buyTroops(informationObjWithoutUserProp).success;
+        it('if handler returns false when information is an empty object', function () {
+            var emptyInformationObj = {};
+            var actual = troopsHandler.buyTroops(emptyInformationObj).success;
 
-        expect(actual).to.equal(false);
-    });
+            expect(actual).to.equal(false);
+        });
 
-    it('if handler returns false when information has no castle property', function() {
-        var informationWithoutCastleProp = { request: { index: 1, amount: 5}, user: { gold: 25000, troops: [10, 15, 2, 1, 3, 4, 0]}};
-        var actual = troopsHandler.buyTroops(informationWithoutCastleProp).success;
+        it('if handler returns false when information has no request property', function () {
+            var informationObjWithoutRequestProp = {  user: { gold: 25000, troops: [10, 15, 2, 1, 3, 4, 0]}, castle: {troopsForSale: [100, 20, 10, 30, 8, 2]}};
+            var actual = troopsHandler.buyTroops(informationObjWithoutRequestProp).success;
 
-        expect(actual).to.equal(false);
-    });
+            expect(actual).to.equal(false);
+        });
 
-    it('if handler returns false when available troops for sale in castle are less than the requested amount', function() {
-        var informationWithCastleTroopsLessThanRequestedAmount = { request: { index: 1, amount: 50}, user: { gold: 25000, troops: [10, 15, 2, 1, 3, 4, 0]}, castle: {troopsForSale: [10, 20, 10, 30, 8, 2]}};
-        var actual = troopsHandler.buyTroops(informationWithCastleTroopsLessThanRequestedAmount).success;
+        it('if handler returns false when information has no user property', function () {
+            var informationObjWithoutUserProp = { request: { index: 1, amount: 5}, castle: {troopsForSale: [100, 20, 10, 30, 8, 2]}};
+            var actual = troopsHandler.buyTroops(informationObjWithoutUserProp).success;
 
-        expect(actual).to.equal(false);
-    });
+            expect(actual).to.equal(false);
+        });
 
-    it('if handler returns false when user does not have enough money to pay for the units he wants to buy', function() {
-        var information = { request: { index: 1, amount: 2}, user: { gold: 250, troops: [10, 15, 2, 1, 3, 4, 0] }, castle: { troopsForSale: [100, 20, 10, 30, 8, 2] }};
+        it('if handler returns false when information has no castle property', function () {
+            var informationWithoutCastleProp = { request: { index: 1, amount: 5}, user: { gold: 25000, troops: [10, 15, 2, 1, 3, 4, 0]}};
+            var actual = troopsHandler.buyTroops(informationWithoutCastleProp).success;
 
-        var actual = troopsHandler.buyTroops(information).success;
+            expect(actual).to.equal(false);
+        });
 
-        expect(actual).to.equal(false);
-    });
+        it('if handler returns false when available troops for sale in castle are less than the requested amount', function () {
+            var informationWithCastleTroopsLessThanRequestedAmount = { request: { index: 1, amount: 50}, user: { gold: 25000, troops: [10, 15, 2, 1, 3, 4, 0]}, castle: {troopsForSale: [10, 20, 10, 30, 8, 2]}};
+            var actual = troopsHandler.buyTroops(informationWithCastleTroopsLessThanRequestedAmount).success;
 
-    it('if handler sets properly the amount of troops for sale left in the castle after user buys certain amount', function(){
-        var information = { request: { index: 1, amount: 2}, user: { gold: 25000, troops: [10, 15, 2, 1, 3, 4, 0] }, castle: { troopsForSale: [100, 20, 10, 30, 8, 2] }};
+            expect(actual).to.equal(false);
+        });
 
-        var expected = information.castle.troopsForSale[information.request.index] - information.request.amount;
+        it('if handler returns false when user does not have enough money to pay for the units he wants to buy', function () {
+            var information = { request: { index: 1, amount: 2}, user: { gold: 250, troops: [10, 15, 2, 1, 3, 4, 0] }, castle: { troopsForSale: [100, 20, 10, 30, 8, 2] }};
+            fakeInfo.user.gold = 0;
+            var actual = troopsHandler.buyTroops(information,gameData).success;
 
-        var result = troopsHandler.buyTroops(information);
-        var troopsAmountLeftInCastle = result.castle.troopsForSale[information.request.index];
+            expect(actual).to.equal(false);
+        });
 
-        expect(troopsAmountLeftInCastle).to.equal(expected);
-    });
+        it('if handler sets properly the amount of troops for sale left in the castle after user buys certain amount', function () {
+            var information = { request: { index: 1, amount: 2}, user: { gold: 25000, troops: [10, 15, 2, 1, 3, 4, 0] }, castle: { troopsForSale: [100, 20, 10, 30, 8, 2] }};
 
-    it('if after buying new troops the amount of certain unit of troops the user owns has been properly increased', function() {
-        var information = { request: { index: 1, amount: 2}, user: { gold: 25000, troops: [10, 15, 2, 1, 3, 4, 0] }, castle: { troopsForSale: [100, 20, 10, 30, 8, 2] }};
+            var expected = information.castle.troopsForSale[information.request.index] - information.request.amount;
 
-        var expected = information.user.troops[information.request.index] + information.request.amount;
+            var result = troopsHandler.buyTroops(information);
+            var troopsAmountLeftInCastle = result.castle.troopsForSale[information.request.index];
 
-        var result = troopsHandler.buyTroops(information);
-        var amountOfTroopUnitsAfterBuyingNewUnits = result.user.troops[information.request.index];
+            expect(troopsAmountLeftInCastle).to.equal(expected);
+        });
 
-        expect(amountOfTroopUnitsAfterBuyingNewUnits).to.equal(expected);
-    });
+        it('if after buying new troops the amount of certain unit of troops the user owns has been properly increased', function () {
+            var information = { request: { index: 1, amount: 2}, user: { gold: 25000, troops: [10, 15, 2, 1, 3, 4, 0] }, castle: { troopsForSale: [100, 20, 10, 30, 8, 2] }};
 
-    it('if after buying new troop units the amount of gold of the user is reduced and set properly', function() {
-        var information = { request: { index: 1, amount: 2}, user: { gold: 25000, troops: [10, 15, 2, 1, 3, 4, 0] }, castle: { troopsForSale: [100, 20, 10, 30, 8, 2] }};
+            var expected = information.user.troops[information.request.index] + information.request.amount;
+            gameData.players.get = function () {
+              return information.user;
+            };
 
-        var expected = information.user.gold - (information.request.amount * 300);
+            var result = troopsHandler.buyTroops(information, gameData);
+            var amountOfTroopUnitsAfterBuyingNewUnits = result.user.troops[information.request.index];
 
-        var result = troopsHandler.buyTroops(information);
-        var amountOfGoldLeftAfterPurchase = result.user.gold;
+            expect(amountOfTroopUnitsAfterBuyingNewUnits).to.equal(expected);
+        });
 
-        expect(amountOfGoldLeftAfterPurchase).to.equal(expected);
+        it('if after buying new troop units the amount of gold of the user is reduced and set properly', function () {
+            var information = { request: { index: 1, amount: 2}, user: { gold: 25000, troops: [10, 15, 2, 1, 3, 4, 0] }, castle: { troopsForSale: [100, 20, 10, 30, 8, 2] }};
+            gameData.players.get = function () {
+                return information.user;
+            };
+
+            var expected = information.user.gold - (information.request.amount * 300);
+
+            var result = troopsHandler.buyTroops(information,gameData);
+            var amountOfGoldLeftAfterPurchase = result.user.gold;
+
+            expect(amountOfGoldLeftAfterPurchase).to.equal(expected);
+        });
     });
 });
